524fd3423832ff6f35bfadf6eca14a29e8e6cd24
YourDaddey38 <96000563+YourDaddey38@users.noreply.github.com>
2022-09-19 13:03:49 -0500

    Add files via upload

---
+++ RuinMod.cs
@@ -0,0 +1,24 @@
+using Terraria.ModLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.GameContent.ItemDropRules;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.DataStructures;
+using Terraria.GameContent.Bestiary;
+using Terraria.ModLoader.IO;
+
+namespace RuinMod
+{
+	public class RuinMod : Mod
+	{
+		public const string ASSET_PATH = "RuinMod/Assets/";
+	}
+}
